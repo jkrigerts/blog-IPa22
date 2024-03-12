879f00b8d3a5ab38dce2eb37ae37cf861823b753
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2024-03-12 09:50:33 +0200

    Refactoring - extraction of views. Beginning

--- index.php
+++ index.php
@@ -20,31 +20,29 @@ if (isset($_GET["category"]) && $_GET["category"] != "") {
   // .= paņem iepriekšējo vērtību un pieliek WHERE klāt ❤️
   $query_string .= " JOIN categories ON posts.category_id = categories.id WHERE categories.name=:category";
   $params[":category"] = $_GET["category"];
-} 
-
+}
 $posts = $db->execute($query_string, $params);
 
-echo "<form>";
-echo "<input name='id'/>";
-echo "<button>Filter by ID</button>";
-echo "</form>";
+
+?>
+
+<form>
+  <input name='id'/>
+  <button>Filter by ID</button>
+</form>
+
+<form>
+  <input name='category' value='<?= ($_GET["category"] ?? '') ?>'/>
+  <button>Filter by Category</button>
+</form>
 
 
-echo "<form>";
-// Null coalescing operator
-echo "<input name='category' value='". ($_GET["category"] ?? '') ."'/>";
+<h1>Posts</h1>
 
-
-echo "<button>Filter by Category</button>";
-echo "</form>";
-
-
-echo "<h1>Posts</h1>";
-
-echo "<ol>";
-foreach($posts as $post) {
-  echo "<li>" . $post["title"] . "</li>";
-}
-echo "</ol>";
+<ol>
+<?php foreach($posts as $post) { ?>
+  <li> <?= $post["title"] ?> </li>
+<?php } ?>
+</ol>
 
 
