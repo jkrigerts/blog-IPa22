77b6a9c0d971a7f8561b3913dcc79dd57c6f4807
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2024-03-16 08:58:51 +0200

    Router extracted

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
 <?php
-$url = parse_url($_SERVER["REQUEST_URI"])["path"];
 
-// Ja lietotājs aiziet uz /,
-// tad parādīt controllers/index.php
-switch($url) {
-  case "":
-  case "/":
-    require "controllers/index.php";
-    break;
-  case "/about":
-    require "controllers/about.php";
-    break;
-  case "/story":
-    require "controllers/story.php";
-    break;
-  default:
-    http_response_code(404);
-    require "controllers/404.php";
-}
+require "router.php";
